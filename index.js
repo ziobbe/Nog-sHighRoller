1ab04225ce1ce0889fbc32505c7da7c37c24d252
Giuseppe Ventura <giuseppe.ventura.bari.dev@gmail.com>
2022-10-25 00:01:44 +0200

    Developement progress

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 const config = require("./config.json");
-const fetch = require("node-fetch")
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const rpgDiceRoller = require('@dice-roller/rpg-dice-roller');
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
 
-function getQuote() {
-	return fetch("https://zenquotes.io/api/kindness")
-		.then(res => {
-			return res.json()
-		})
-    .then(data => {
-		return data[0]["q"] + " -" + data[0]["a"]
-    })
-}
+// function getQuote() {
+// 	return fetch("https://zenquotes.io/api/kindness")
+// 		.then(res => {
+// 			return res.json()
+// 		})
+//     .then(data => {
+// 		return data[0]["q"] + " -" + data[0]["a"]
+//     })
+// }
 
+console.log("Loggin with token: "+config.BOT_TOKEN);
 const prefix = "!";
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
 client.on("messageCreate", function(message) {
 	//guardians
 	if (message.author.bot) return; //let's ignore bots.
@@ -25,11 +36,15 @@ client.on("messageCreate", function(message) {
 		const timeTaken = Date.now() - message.createdTimestamp;
 		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
 	}
-	if (command === "sum") {
-		const numArgs = args.map(x => parseFloat(x));
-		const sum = numArgs.reduce((counter, x) => counter += x);
-		message.reply(`The sum of all the arguments you provided is ${sum}!`);
+	if (command==="r"){
+		const roll= new rpgDiceRoller(commandBody);
+		message.reply(" ${roll}!");
 	}
+	// if (command === "sum") {
+	// 	const numArgs = args.map(x => parseFloat(x));
+	// 	const sum = numArgs.reduce((counter, x) => counter += x);
+	// 	message.reply(`The sum of all the arguments you provided is ${sum}!`);
+	// }
 	if (command === "inspiration"){
 		message.reply(getQuote());
 	}
